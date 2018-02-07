71a0e2525bf1c6e8ba3bf97c730663a7d9ab7778
Jeff Crowder <jeffrey_s_crowder@whirlpool.com>
2018-02-07 17:53:35 -0600

    this is woking but needs to be changed to a batched query

--- GoogleSheetsAPIExample/Program.cs
+++ GoogleSheetsAPIExample/Program.cs
@@ -54,12 +54,15 @@ namespace SheetsQuickstart
             // Define request parameters.
             String spreadsheetId = "1c54Cy_B43h5-nmE7r6Slvj2w8Pl0XFxgaWpTxO9s9So";
             String readRange = "ReadData!A1:F";
+
             SpreadsheetsResource.ValuesResource.GetRequest request =
                     service.Spreadsheets.Values.Get(spreadsheetId, readRange);
 
             // Prints the data from a sample spreadsheet:
             // https://docs.google.com/spreadsheets/d/1c54Cy_B43h5-nmE7r6Slvj2w8Pl0XFxgaWpTxO9s9So/edit#gid=0
+
             ValueRange response = request.Execute();
+
             IList<IList<Object>> values = response.Values;
             if (values != null && values.Count > 0)
             {
@@ -76,22 +79,40 @@ namespace SheetsQuickstart
             }
 
             //Write some data
-            String writeRange = "WriteData!A1:C3";
+            String writeRange = "WriteData!A1:ZZ";
             ValueRange valueRange = new ValueRange();
             valueRange.MajorDimension = "ROWS";//"ROWS";//"COLUMNS"
 
-           // here is the actual data to be sent to sheet
-            var headerList = new List<object>() { "CAL", "FPY", "AY" };
+            // here is the actual data to be sent to sheet
+            //var headerList = new List<object>() { "CAL", "FPY", "AY" };
+            var headerList = new List<object>() { 
+            "ID","DTStamp","DTShiftStart","ModelNbr","SerialNbr","PassFail","LineNbr","ShiftNbr","Computer","Word40","Word41","Word42"
+            ,"Word43","Word44","Word45","Word46","Word47","Word48","Word49","Word50","Word51","Word52","Word53","Word54","Word55","Word56"
+            ,"Word57","Word58","Word59","Word60","Word61","Word62","Word63","Word64","Word65","Word66","Word67","Word68","Word69","Word70"
+            ,"Word71","Word72","Word73","Word74","Word75","Word76","Word77","Word78","Word79","Word80"};
 
-            var dataList = new List<object>() { 1, 2, 3 };
-
-            valueRange.Values = new List<IList<object>> { headerList, dataList };
+            //var dataList = new List<object>() { 1, 2, 3 };
+            //This is a fucking HACK
+            var dataList = new List<object>()
             
+            {
+                1,1,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,
+                null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null
+            };
+            
+            //valueRange.Values = new List<IList<object>> { headerList };
+
+            //API method to clear the sheet
+            ClearValuesRequest clearValuesRequest = new ClearValuesRequest();
+            SpreadsheetsResource.ValuesResource.ClearRequest cr = service.Spreadsheets.Values.Clear(clearValuesRequest, spreadsheetId, writeRange);
+            ClearValuesResponse clearResponse = cr.Execute();
+
+            //API method to update the sheet
+            valueRange.Values = new List<IList<object>> { headerList };
             SpreadsheetsResource.ValuesResource.UpdateRequest update = service.Spreadsheets.Values.Update(valueRange, spreadsheetId, writeRange);
             update.ValueInputOption = SpreadsheetsResource.ValuesResource.UpdateRequest.ValueInputOptionEnum.RAW;
             UpdateValuesResponse result;
-            //result = update.Execute();
-
+            result = update.Execute();
 
             //start the readKey in a new thread 
             Task.Factory.StartNew(() =>
@@ -106,7 +127,7 @@ namespace SheetsQuickstart
             Random r = new Random();
 
             //Loop and update the data until enter key is hit to exit
-            do
+           /* do
             {
                 //clear the data
                 dataList.RemoveRange(0, 3);
@@ -127,49 +148,94 @@ namespace SheetsQuickstart
                 //wait
                 System.Threading.Thread.Sleep(3000);
             } while (!exit);
-
-
-
+            */
 
             SqlConnection sqlConnection = new SqlConnection("Data Source=tul-mssql;Initial Catalog=Division;User ID=tqisadmin;Password=admin2k");
             //SqlConnection sqlConnection = new SqlConnection("Data Source=tul-sqldev;Initial Catalog=TulQual;User ID=tqisadmin;Password=admin2k");
             SqlCommand cmd = new SqlCommand();
             SqlDataReader reader;
-
+            /*
             cmd.CommandText = "Select LineNbr,passfail,cast(count(*) * 100.0 / sum(count(*))over(partition BY linenbr) as decimal (10, 2)) pctTotals " +
             "FROM[Division].[dbo].[asyTestRecords]" +
             "WHERE Computer = 'LN'and shiftNbr = 1 and datepart(DAYOFYEAR, [DTShiftStart]) = DATEPART(DAyofyear, current_timestamp)" +
             "group BY linenbr,shiftnbr,passfail order bY LineNbr, passfail desc";
-
+            */
+            ///*
+            cmd.CommandText = "SELECT TOP 1000 [ID],[DTStamp],[DTShiftStart],[ModelNbr],[SerialNbr],[PassFail],[LineNbr],[ShiftNbr],[Computer],[Word40],[Word41],[Word42]" +
+            ",[Word43],[Word44],[Word45],[Word46],[Word47],[Word48],[Word49],[Word50],[Word51],[Word52],[Word53],[Word54],[Word55],[Word56]" +
+            ",[Word57],[Word58],[Word59],[Word60],[Word61],[Word62],[Word63],[Word64],[Word65],[Word66],[Word67],[Word68],[Word69],[Word70]" +
+            ",[Word71],[Word72],[Word73],[Word74],[Word75],[Word76],[Word77],[Word78],[Word79],[Word80] " +
+            "FROM[Division].[dbo].[asyTestRecords] where LineNbr = 2 and computer = 'LN' and dtstamp > '2/1/2018 5:00' order by dtstamp desc";
+            //*/
             //cmd.CommandText = "SELECT[LineNbr],[pctTotals] FROM[TulQual].[dbo].[vwLiveFPYFirstShift] WHERE passfail = 'P'ORDER BY [LineNbr]";
 
             cmd.CommandType = CommandType.Text;
             cmd.Connection = sqlConnection;
 
+            Console.WriteLine("Open the SQL connection");
             sqlConnection.Open();
             cmd.CommandTimeout = 60;
+            Console.WriteLine("Please wait while reading data from SQL");
             reader = cmd.ExecuteReader();
 
             // Data is accessible through the DataReader object here.
+            //Console.WriteLine("Start writing data");
+
+            ValueRange valueDataRange = new ValueRange();
+            valueDataRange.MajorDimension = "ROWS";
+
+            valueDataRange.Values = new List<IList<object>> { dataList };
+
+            //API to append data to sheet
+            SpreadsheetsResource.ValuesResource.AppendRequest appendRequest = service.Spreadsheets.Values.Append(valueDataRange, spreadsheetId, writeRange);
+            appendRequest.ValueInputOption = SpreadsheetsResource.ValuesResource.AppendRequest.ValueInputOptionEnum.RAW;
+            appendRequest.InsertDataOption = SpreadsheetsResource.ValuesResource.AppendRequest.InsertDataOptionEnum.INSERTROWS;
+
             if (reader.HasRows)
             {
-                Console.Write("\r\n");
+                Console.WriteLine("{0}",reader.FieldCount);
+
+                Object[] colValues = new Object[reader.FieldCount];
+
+                int throttleCount = 0;
                 int cnt = 0;
                 while (reader.Read())
                 {
-                    Console.WriteLine("\t{0}\t{1}\t{2}", reader.GetSqlInt16(0).ToSqlString(), reader.GetSqlString(1), reader.GetSqlDecimal(2).ToSqlString());
-                    //Console.WriteLine("{0}\t{1}", reader.GetSqlInt16(0).ToSqlString(), reader.GetSqlDecimal(1).ToSqlString());
+                    dataList.Clear();
+
+                    int fieldCount = reader.GetValues(colValues);
+                    for (int i = 0; i < fieldCount; i++)
+                    {
+                        dataList.Add(colValues[i]);
+                    }
+                    //This is the GOOGLE query Throttle they only allow 500 writes per 100 sec
+                    System.Threading.Thread.Sleep(500);
+                    //Console.WriteLine("\t{0}\t{1}\t{2}", reader.GetSqlInt32(0).ToSqlString(), reader.GetSqlDateTime(1), reader.GetSqlDateTime(2));
+
+                    try
+                    {
+                        AppendValuesResponse appendValueResponse = appendRequest.Execute();
+                    }
+                    catch(Exception e)
+                    {
+                        //Console.WriteLine("Whoa buddy caught {0}", e);
+                        Console.WriteLine("Whoa buddy slowdown {0}",throttleCount);
+                        System.Threading.Thread.Sleep(1000);
+                        throttleCount++;
+                    }
                     cnt++;
-                    //Console.Write(cnt);
                 }
-                System.Threading.Thread.Sleep(3000);
             }
             else
             {
                 Console.WriteLine("No rows found.");
             }
-            reader.Close();
+            
+            // sit here and wait for a while
+            Console.WriteLine("Done waiting to close reader and SQL");
+            System.Threading.Thread.Sleep(3000);
 
+            reader.Close();
             sqlConnection.Close();
         }
     }
