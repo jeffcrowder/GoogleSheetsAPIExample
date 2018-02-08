e9d200fda1b05007be2fd7c64e141f47a42d47f0
Jeff Crowder <jeffrey_s_crowder@whirlpool.com>
2018-02-08 15:15:31 -0600

    did some house cleaning. Still need to make this OO and get rid of the Update call and replace with ful Batch update

--- GoogleSheetsAPIExample/Program.cs
+++ GoogleSheetsAPIExample/Program.cs
@@ -21,7 +21,7 @@ namespace SheetsQuickstart
         // at ~/.credentials/sheets.googleapis.com-dotnet-quickstart.json
         static string[] Scopes = { SheetsService.Scope.Spreadsheets };
         static string ApplicationName = "Google Sheets API .NET Quickstart";
-        static volatile bool exit = false;
+        //static volatile bool exit = false;
 
         static void Main(string[] args)
         {
@@ -53,56 +53,29 @@ namespace SheetsQuickstart
 
             // Define request parameters.
             String spreadsheetId = "1c54Cy_B43h5-nmE7r6Slvj2w8Pl0XFxgaWpTxO9s9So";
-            String readRange = "ReadData!A1:F";
-
-            //API to get values from a sheet
-            SpreadsheetsResource.ValuesResource.GetRequest request = service.Spreadsheets.Values.Get(spreadsheetId, readRange);
-            ValueRange response = request.Execute();
-
-            IList<IList<Object>> values = response.Values;
-            if (values != null && values.Count > 0)
-            {
-                foreach (var row in values)
-                {
-                    // Print columns A through E, which correspond to indices 0 through 5.
-                    //Need to seperate the headers from the data
-                    Console.WriteLine("{0},         {1},    {2},    {3},    {4},    {5}", row[0], row[1], row[2], row[3], row[4], row[5]);
-                }
-            }
-            else
-            {
-                Console.WriteLine("No data found.");
-            }
-
-            
 
             // here is the actual data to be sent to sheet
-            //var headerList = new List<object>() { "CAL", "FPY", "AY" };
-            var headerList = new List<object>() { 
+            List<object> headerList = new List<object>() {
             "ID","DTStamp","DTShiftStart","ModelNbr","SerialNbr","PassFail","LineNbr","ShiftNbr","Computer","Word40","Word41","Word42"
             ,"Word43","Word44","Word45","Word46","Word47","Word48","Word49","Word50","Word51","Word52","Word53","Word54","Word55","Word56"
             ,"Word57","Word58","Word59","Word60","Word61","Word62","Word63","Word64","Word65","Word66","Word67","Word68","Word69","Word70"
             ,"Word71","Word72","Word73","Word74","Word75","Word76","Word77","Word78","Word79","Word80"};
 
-            
-            //var dataList = new List<object>() { 1, 2, 3 };
-            //This is a fucking HACK need to add an initialize dataList method
-            var dataList = new List<object>()
-            
-            {
-                1,1,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,
-                null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null
-            };
-            
+            //var dataList = new List<object>();
+
             //Write some data
             String writeRange = "WriteData!A1:ZZ";
             ValueRange valueRange = new ValueRange();
             valueRange.MajorDimension = "ROWS";
 
+            Console.WriteLine("Clear the Sheet");
+
             //API method to clear the sheet
             ClearValuesRequest clearValuesRequest = new ClearValuesRequest();
             SpreadsheetsResource.ValuesResource.ClearRequest cr = service.Spreadsheets.Values.Clear(clearValuesRequest, spreadsheetId, writeRange);
             ClearValuesResponse clearResponse = cr.Execute();
+
+            Console.WriteLine("Delete all rows in Sheet");
 
             //API method to batch update
             DimensionRange dr = new DimensionRange
@@ -127,6 +100,8 @@ namespace SheetsQuickstart
             SpreadsheetsResource.BatchUpdateRequest bRequest = service.Spreadsheets.BatchUpdate(requestBody, spreadsheetId);
             BatchUpdateSpreadsheetResponse busr = bRequest.Execute();
 
+            Console.WriteLine("Write the Headers to the Sheet");
+
             //API method to update the sheet
             valueRange.Values = new List<IList<object>> { headerList };
             SpreadsheetsResource.ValuesResource.UpdateRequest update = service.Spreadsheets.Values.Update(valueRange, spreadsheetId, writeRange);
@@ -134,61 +109,15 @@ namespace SheetsQuickstart
             UpdateValuesResponse result;
             result = update.Execute();
 
-            /*
-            //start the readKey in a new thread 
-            Task.Factory.StartNew(() =>
-            {
-                while (Console.ReadKey(true).Key != ConsoleKey.Enter) ;
-                exit = true;
-            });
-
-            Console.Write("Press <Enter> to exit...\r\n");
-            int x = Console.CursorLeft;
-            int y = Console.CursorTop;
-            Random r = new Random();
-
-            //Loop and update the data until enter key is hit to exit
-            do
-            {
-                //clear the data
-                dataList.RemoveRange(0, 3);
-
-                //refresh the data
-                dataList.Add(r.Next(80,100));
-                dataList.Add(r.Next(75,99));
-                dataList.Add(r.Next(90,99));
-
-                //update data via the API
-                result = update.Execute();
-
-                Console.Write("Updated with: " + dataList.ElementAt(0) + " : " + dataList.ElementAt(1) + " : " + dataList.ElementAt(2));
-
-                // Restore previous position
-                Console.SetCursorPosition(x, y);
-
-                //wait
-                System.Threading.Thread.Sleep(3000);
-            } while (!exit);
-            */
-            
             SqlConnection sqlConnection = new SqlConnection("Data Source=tul-mssql;Initial Catalog=Division;User ID=tqisadmin;Password=admin2k");
-            //SqlConnection sqlConnection = new SqlConnection("Data Source=tul-sqldev;Initial Catalog=TulQual;User ID=tqisadmin;Password=admin2k");
             SqlCommand cmd = new SqlCommand();
             SqlDataReader reader;
-            /*
-            cmd.CommandText = "Select LineNbr,passfail,cast(count(*) * 100.0 / sum(count(*))over(partition BY linenbr) as decimal (10, 2)) pctTotals " +
-            "FROM[Division].[dbo].[asyTestRecords]" +
-            "WHERE Computer = 'LN'and shiftNbr = 1 and datepart(DAYOFYEAR, [DTShiftStart]) = DATEPART(DAyofyear, current_timestamp)" +
-            "group BY linenbr,shiftnbr,passfail order bY LineNbr, passfail desc";
-            */
-            ///*
+  
             cmd.CommandText = "SELECT TOP 1000 [ID],[DTStamp],[DTShiftStart],[ModelNbr],[SerialNbr],[PassFail],[LineNbr],[ShiftNbr],[Computer],[Word40],[Word41],[Word42]" +
             ",[Word43],[Word44],[Word45],[Word46],[Word47],[Word48],[Word49],[Word50],[Word51],[Word52],[Word53],[Word54],[Word55],[Word56]" +
             ",[Word57],[Word58],[Word59],[Word60],[Word61],[Word62],[Word63],[Word64],[Word65],[Word66],[Word67],[Word68],[Word69],[Word70]" +
             ",[Word71],[Word72],[Word73],[Word74],[Word75],[Word76],[Word77],[Word78],[Word79],[Word80] " +
             "FROM[Division].[dbo].[asyTestRecords] where LineNbr = 2 and computer = 'LN' and dtstamp > '2/1/2018 5:00' order by dtstamp desc";
-            //*/
-            //cmd.CommandText = "SELECT[LineNbr],[pctTotals] FROM[TulQual].[dbo].[vwLiveFPYFirstShift] WHERE passfail = 'P'ORDER BY [LineNbr]";
 
             cmd.CommandType = CommandType.Text;
             cmd.Connection = sqlConnection;
@@ -200,11 +129,10 @@ namespace SheetsQuickstart
             reader = cmd.ExecuteReader();
 
             // Data is accessible through the DataReader object here.
-            //Console.WriteLine("Start writing data");
-
             ValueRange valueDataRange = new ValueRange();
             valueDataRange.MajorDimension = "ROWS";
 
+            var dataList = new List<object>();
             valueDataRange.Values = new List<IList<object>> { dataList };
 
             //API to append data to sheet
@@ -214,34 +142,31 @@ namespace SheetsQuickstart
 
             if (reader.HasRows)
             {
-                Console.WriteLine("{0}",reader.FieldCount);
-
+                //Console.WriteLine("{0}",reader.FieldCount);
                 Object[] colValues = new Object[reader.FieldCount];
 
                 int throttleCount = 0;
                 int cnt = 0;
                 while (reader.Read())
                 {
+                    //This logic is flawed. If we get hit by the quota then the data row gets lost the next time this runs.
                     dataList.Clear();
-
-                    int fieldCount = reader.GetValues(colValues);
-                    for (int i = 0; i < fieldCount; i++)
+                    for (int i = 0; i < reader.GetValues(colValues); i++)
                     {
                         dataList.Add(colValues[i]);
                     }
-                    //This is the GOOGLE query Throttle they only allow 500 writes per 100 sec
-                    System.Threading.Thread.Sleep(1000);
-                    //Console.WriteLine("\t{0}\t{1}\t{2}", reader.GetSqlInt32(0).ToSqlString(), reader.GetSqlDateTime(1), reader.GetSqlDateTime(2));
 
                     try
                     {
+                        //This is the GOOGLE query Throttle they only allow 500 writes per 100 sec
+                        System.Threading.Thread.Sleep(20);
                         AppendValuesResponse appendValueResponse = appendRequest.Execute();
+                        Console.WriteLine("Writing to Sheet: row{0}", cnt);
                     }
                     catch(Exception e)
                     {
-                        //Console.WriteLine("Whoa buddy caught {0}", e);
                         Console.WriteLine("Whoa buddy slowdown {0}",throttleCount);
-                        System.Threading.Thread.Sleep(1000);
+                        System.Threading.Thread.Sleep(3000);
                         throttleCount++;
                     }
                     cnt++;
